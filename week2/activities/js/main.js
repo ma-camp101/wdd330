bd5cdff72b880732e33dcfb1bc20d6010f63e40f
ma-camp101 <cam20032@byui.edu>
2022-01-16 08:54:37 -0500

    changes

---
+++ week2/activities/js/main.js
@@ -0,0 +1,8 @@
+function fizzBuzz() {
+    for (let n = 1; n <= 100; n++) {
+        let output = "";
+        if (n % 3 == 0) output += "Fizz";
+        if (n % 5 == 0) output += "Buzz";
+    }    
+    document.getElementById("fizzy").innerHTML = (output || n);
+}
