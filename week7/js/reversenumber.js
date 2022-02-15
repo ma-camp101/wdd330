06caa6f34b3635cc83fb6889ee497705f8dee450
ma-camp101 <cam20032@byui.edu>
2022-02-15 16:52:54 -0500

    changes

--- week7/js/reversenumber.js
+++ week7/js/reversenumber.js
@@ -1,3 +1,4 @@
+// Write a JavaScript function that reverse a number.
 function reverseNumber() {
     let number = Number(document.getElementById("input_number").value);
     let reverse = Number(String(number).split('').reverse().join(''));
