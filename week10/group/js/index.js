59dd75a78d0e79f339293f36355aca2d103c4868
ma-camp101 <cam20032@byui.edu>
2022-03-09 23:27:30 -0500

    changes

---
+++ week10/group/js/index.js
@@ -0,0 +1,5 @@
+import QuakesController from "./quakesController.js";
+
+const quakeController = new QuakesController('#quakeList')
+
+quakeController.init();
---
+++ week10/group/js/main.js
@@ -0,0 +1,20 @@
+import { getJSON, getLocation } from "./utilities.js";
+
+const baseUrl =
+  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-02-02";
+
+let quakes = [];
+
+async function showQuakes() {
+    // get the current location
+    const location = await initPos();
+    // get the list of quakes for the location
+    quakes = await getQuakesForLocation(location);
+    // render the list to the list element
+    const listElement = document.querySelector("#quakeList");
+    listElement.innerHTML = generateListMarkup( quakes.features, earthquakeListTemplate);
+  
+    // attach a listener to the quakes that will listen for a click and render out details about the quake
+    listElement.addEventListener("click", earthQuakeClickHandler);
+  }
+       
