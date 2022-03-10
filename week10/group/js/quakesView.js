b3d420d939c7d106116fc2aceba1506a797dd9d6
ma-camp101 <cam20032@byui.edu>
2022-03-10 12:05:07 -0500

    changes

--- week10/group/js/quakesView.js
+++ week10/group/js/quakesView.js
@@ -23,8 +23,8 @@ class QuakesView {
     const quakeProperties = Object.entries(quake.properties);
     element.innerHTML = quakeProperties.map(item => {
         if (item[0] === 'time' || item[0] === 'updated') {
-          return <li>${item[0]}: ${new Date(item[1])}</li>;
-        } else return <li>${item[0]}: ${item[1]}</li>;
+          return `<li>${item[0]}: ${new Date(item[1])}</li>`;
+        } else return `<li>${item[0]}: ${item[1]}</li>`;
       })
       .join('');
       element.append(backButton);
